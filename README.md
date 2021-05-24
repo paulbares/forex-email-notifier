a84874d1785a6df7282d7c2fee545f1b59ebc9de
paulbares <bares.paul@gmail.com>
2021-05-24 15:58:34 +0400

    Add first version
    Add github action
    
    Fix exception swallowed
    
    Handle promise rejection
    
    Fix GA conf
    
    Restore env in GA
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Add more logs
    
    Update notifier.yml

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # forex-email-notifier
 Send forex rates periodically to a list of recipients/subscribers 
+
+## Configuration
+- Forex rate is fetched from https://fixer.io/. You need an API access key to be able to fetch anything.
+- A smtp server and a mail client. You can use Gmail for instance. 
+
+The following parameters need to be set as environment variables:
+
+```javascript
+const access_key=process.env.FOREX_EMAIL_NOTIFIER_AK; // see usage https://fixer.io/documentation
+const user=process.env.FOREX_EMAIL_NOTIFIER_USER; // see usage https://nodemailer.com/about/#example
+const pass=process.env.FOREX_EMAIL_NOTIFIER_PASSWORD; // see usage https://nodemailer.com/about/#example
+const receivers=process.env.FOREX_EMAIL_NOTIFIER_RECIPIENTS; // usage see https://nodemailer.com/about/#example
+```
+
+## Run
+Once configured, simply run:
+
+```javascript
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+const nodemailer = require("nodemailer");
+const axios = require('axios');
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  const access_key=process.env.FOREX_EMAIL_NOTIFIER_AK;
+  const user=process.env.FOREX_EMAIL_NOTIFIER_USER;
+  const pass=process.env.FOREX_EMAIL_NOTIFIER_PASSWORD;
+  const receivers=process.env.FOREX_EMAIL_NOTIFIER_RECIPIENTS;
+
+  const result = await axios.get(`http://data.fixer.io/api/latest?access_key=${access_key}&base=EUR&symbols=AED`);
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user
+      pass
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: receivers, // list of receivers
+    subject: `EUR to AED rate as of ${result.data.date}: ${result.data.rates.AED}`,
+    text: "No content", // plain text body
+    html: "No content", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
